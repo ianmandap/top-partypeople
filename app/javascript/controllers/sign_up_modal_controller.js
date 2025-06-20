bbc9494af3ec7bf4f20f4ee54f94923f78240124
ianmandap <mandap.fmg@gmail.com>
2025-06-20 19:35:26 +0800

    Fix unregistered user flow for creating an event

--- app/javascript/controllers/sign_up_modal_controller.js
+++ app/javascript/controllers/sign_up_modal_controller.js
@@ -2,10 +2,22 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="sign-up-modal"
 export default class extends Controller {
+  newUserClick(e) {
+    const eventsForm = document.getElementById('events-form');
+
+    // use Constraint Validation API
+    if (eventsForm.checkValidity()) {
+      // continue click event
+    } else {
+      e.preventDefault();
+      eventsForm.reportValidity();
+    }
+  }
+
   newUserSubmit(e) {
     if (e.detail.success) {
-      const eventsForm = document.getElementById('events-form')
-      eventsForm.submit();
+      const eventsForm = document.getElementById('events-form');
+      eventsForm.requestSubmit();
     }
   }
 }
